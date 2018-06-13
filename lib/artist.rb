95f30714303f6e1b96161a741518650f24d2bd40
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-06-13 00:47:04 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,10 +8,14 @@ class Artist
     @songs = []
   end
 
-
   def save
     @@all << self
   end
+
+  def add_song(song)
+    @songs << song
+  end
+
 
   def self.find_or_create_by_name(name)
     if self.all.detect {|artist| artist.name == name}
--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -13,7 +13,7 @@ class MP3Importer
     self.files.each do |filename|
       Song.new_by_filename(filename)
     end
-  end 
+  end
 end
 
 
