6fe31fccc74f12740b37be324e54826ffa6467d9
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-06-13 00:51:25 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -21,8 +21,20 @@ class Artist
   end
 
   def self.find_or_create_by_name(name)
-    @@all.detect {|artist| artist.name == name} ? @@all.detect : Artist.new(name)
+    art = @@all.find { |artist| artist.name == name }
+    if art == nil
+      new_artist = Artist.new(name)
+      new_artist.save
+      new_artist
+    else
+      art
+    end
   end
+
+
+#  def self.find_or_create_by_name(name)
+#    @@all.detect {|artist| artist.name == name} ? @@all.detect : self.new(name)
+#  end
 
   def print_songs
     @songs.each { |song| puts song.name }
