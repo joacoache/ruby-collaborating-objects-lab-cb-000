6d4876f2584f3f51e2666b116a81510fc16dece0
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-06-13 00:45:00 +0000

    automatically backed up by learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,26 @@
+class MP3Importer
+  attr_accessor :path, :files
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    @files = Dir.entries(@path)
+    @files.grep(/mp3/)
+  end
+
+  def import
+    self.files.each do |filename|
+      Song.new_by_filename(filename)
+    end
+  end 
+end
+
+
+
+#MP3Importer.new('./db/mp3s').import
+
+#Dir["/path/to/search/*"]
+
+#Dir['./db/mp3s']
